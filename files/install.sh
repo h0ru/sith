3ec4599cafce7f578464a56e16caccfa6c3958a6
h0ru <117091833+h0ru@users.noreply.github.com>
2023-06-25 19:14:33 -0300

    Create install.sh

---
+++ files/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#Colors:
+        Rd='\033[0;31m'    # Red
+        Be='\033[0;34m'   # Blue
+        Gn='\033[0;32m'  # Green
+        We='\033[0;97m'  # White
+
+echo -e "$We["$Rd"SITH Downloading"$We"]"
+
+echo -e "$Be>$We sith"
+wget -q "https://raw.githubusercontent.com/h0ru/sith/main/sith" -O sith ; chmod +x sith;
+echo -e "$Gn finished $We"
+
+echo -e "$Be>$We chacal-c2"
+mkdir -p ~/.sith/chacal-c2 ; wget -q "https://raw.githubusercontent.com/h0ru/sith/main/chacal" -O chacal ; chmod +x chacal ; mv ./chacal ~/.sith/chacal-c2/ ; touch ~/.sith/chacal-c2/targets.txt;
+echo -e "$Gn finished $We"
+
+echo -e "$Be>$We myrshell"
+wget -q "https://raw.githubusercontent.com/h0ru/myrshell/main/myrshell" -O myrshell ; chmod +x ./myrshell ; mv ./myrshell ~/.sith/
+echo -e "$Gn finished $We"
+
+echo -e "$Be>$We Move sith to /usr/local/bin/"
+sudo mv sith /usr/local/bin
+echo -e "$Gn finished $We"
